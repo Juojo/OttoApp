cfb677b2701701dc6d3a98fbf4ac7345967871d0
Juojo <80736129+Juojo@users.noreply.github.com>
2025-10-11 23:14:26 -0300

    Agrega endpoint para crear usuarios

--- app/servidor/app.py
+++ app/servidor/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/hello")
+@app.route("/api/hello")
 def hello():
     return "Hello, World!"
+
+@app.route("/api/crearUsuario", methods = ['POST'])
+def crearUsuario():
+    usuario = request.get_json()
+    print(usuario)
+
+    return jsonify({
+        "success": True,
+        "message": "Usuario creado exitosamente",
+        "usuario": usuario
+    }), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
