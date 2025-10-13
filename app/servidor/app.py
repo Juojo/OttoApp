86eff02cff338049a7146a949f3f6b275558fc00
Juojo <80736129+Juojo@users.noreply.github.com>
2025-10-13 16:07:05 -0300

    Ignora warnings de app.py

--- app/servidor/app.py
+++ app/servidor/app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from dotenv import load_dotenv
+from flask import Flask, request, jsonify # type: ignore
+from flask_cors import CORS  # type: ignore
+from dotenv import load_dotenv # type: ignore
 import os
 
 import usuarios.register
